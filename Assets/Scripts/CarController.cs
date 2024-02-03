3dfc2d2f91ae6ab1e33f98c936bdfcc12c84f20b
Anmol-1903 <anmolmittal1903@gmail.com>
2024-02-04 00:16:09 +0530

    Car Controller

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,140 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class CarController : MonoBehaviour
+{
+    [SerializeField] float _topSpeed = 50f;
+    [SerializeField] float _maxSteerAngle = 30f;
+    [SerializeField] float _torque = 500f;
+    [SerializeField] float _brakingForce = 1000f;
+
+    [Header("Wheels Colliders")]
+    [SerializeField] WheelCollider _fl;
+    [SerializeField] WheelCollider _fr;
+    [SerializeField] WheelCollider _rl;
+    [SerializeField] WheelCollider _rr;
+
+    [Header("Wheels Transforms")]
+    [SerializeField] Transform _flT;
+    [SerializeField] Transform _frT;
+    [SerializeField] Transform _rlT;
+    [SerializeField] Transform _rrT;
+
+    [Header("DriveType")]
+    [SerializeField] bool _front;
+    [SerializeField] bool _rear;
+
+    PlayerController playerController;
+    Rigidbody rb;
+    float dotProduct, _vertical;
+
+    private void Awake()
+    {
+        playerController = new PlayerController();
+        rb = GetComponent<Rigidbody>();
+    }
+    private void OnEnable()
+    {
+        playerController.Enable();
+        playerController.Drive.Accelarate.performed += Accelarate_performed;
+        playerController.Drive.Accelarate.canceled += Accelarate_canceled;
+        playerController.Drive.Steer.performed += Steer_performed;
+        playerController.Drive.Steer.canceled += Steer_canceled;
+        playerController.Drive.Handbrake.performed += Handbrake_performed;
+        playerController.Drive.Handbrake.canceled += Handbrake_canceled;
+    }
+    private void OnDisable()
+    {
+        playerController.Disable();
+        playerController.Drive.Accelarate.performed -= Accelarate_performed;
+        playerController.Drive.Accelarate.canceled -= Accelarate_canceled;
+        playerController.Drive.Steer.performed -= Steer_performed;
+        playerController.Drive.Steer.canceled -= Steer_canceled;
+        playerController.Drive.Handbrake.performed -= Handbrake_performed;
+        playerController.Drive.Handbrake.canceled -= Handbrake_canceled;
+    }
+    private void FixedUpdate()
+    {
+        CarDrive();
+        UpdateWheelPos(_fl, _flT);
+        UpdateWheelPos(_fr, _frT);
+        UpdateWheelPos(_rl, _rlT);
+        UpdateWheelPos(_rr, _rrT);
+    }
+    void CarDrive()
+    {
+        Vector3 carVelocity = rb.velocity;
+        dotProduct = Vector3.Dot(transform.forward, carVelocity);
+        if ((_vertical < 0 && dotProduct > 0.1f) || (_vertical > 0 && dotProduct < -0.1f))
+        {
+            _fl.brakeTorque = _brakingForce;
+            _fr.brakeTorque = _brakingForce;
+            _rl.brakeTorque = _brakingForce;
+            _rr.brakeTorque = _brakingForce;
+        }
+        else
+        {
+            _fl.brakeTorque = 0;
+            _fr.brakeTorque = 0;
+            _rl.brakeTorque = 0;
+            _rr.brakeTorque = 0;
+        }
+        if (dotProduct < _topSpeed)
+        {
+            if (_front)
+            {
+                _fl.motorTorque = _vertical * _torque;
+                _fr.motorTorque = _vertical * _torque;
+            }
+            if (_rear)
+            {
+                _rl.motorTorque = _vertical * _torque;
+                _rr.motorTorque = _vertical * _torque;
+            }
+        }
+    }
+
+    void UpdateWheelPos(WheelCollider c, Transform w)
+    {
+        c.GetWorldPose(out Vector3 pos, out Quaternion rot);
+        w.position = pos;
+        w.rotation = rot;
+    }
+
+    private void Handbrake_canceled(InputAction.CallbackContext obj)
+    {
+        _rl.brakeTorque = 0;
+        _rr.brakeTorque = 0;
+    }
+
+    private void Steer_canceled(InputAction.CallbackContext obj)
+    {
+        _fl.steerAngle = 0;
+        _fr.steerAngle = 0;
+    }
+
+    private void Accelarate_canceled(InputAction.CallbackContext obj)
+    {
+        _fl.motorTorque = 0;
+        _fr.motorTorque = 0;
+        _rl.motorTorque = 0;
+        _rr.motorTorque = 0;
+    }
+
+    private void Handbrake_performed(InputAction.CallbackContext obj)
+    {
+        _rl.brakeTorque = _brakingForce;
+        _rr.brakeTorque = _brakingForce;
+    }
+
+    private void Steer_performed(InputAction.CallbackContext obj)
+    {
+        float _horizontal = obj.ReadValue<float>();
+        _fl.steerAngle = _horizontal * _maxSteerAngle;
+        _fr.steerAngle = _horizontal * _maxSteerAngle;
+    }
+
+    private void Accelarate_performed(InputAction.CallbackContext obj)
+    {
+        _vertical = obj.ReadValue<float>();
+    }
+}
